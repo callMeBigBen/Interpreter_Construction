d487a9b0ce8db0e8baed3a3b4f4eef13764aa6f9
seizerccc <907839891@qq.com>
2018-11-07 11:09:32 +0800

    Update Token.java
    修改token类，增加put方法和line属性

--- Token.java
+++ Token.java
@@ -3,13 +3,14 @@ package syntax;
 public class Token {
 	String content ;
 	int type;
-	
+	int line;
 	Token(){
 		
 	}
 	
-	public void put(String _token,int _type){
+	public void put(String _token,int _type,int _line){
 		content = _token;
 		type = _type;
+		line = _line;
 	}
 }
